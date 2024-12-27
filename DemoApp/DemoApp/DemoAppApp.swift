5cfe437d55764809fc42ccb75efd80bdf450616a
Ricardo CORIN <ricardocorin@mac.home>
2024-12-27 14:25:32 -0300

    demoapp swiftui

---
+++ DemoApp/DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Ricardo CORIN on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
